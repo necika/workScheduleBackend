5e2aa82ed0f22083c35cff29d7a06929740696dd
Nemanja <nemanja.salipurevic@gmail.com>
2020-08-23 15:51:53 +0200

    data sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+--users
+
+INSERT IGNORE INTO `diplomski`.`my_user` (`id`, `email`, `password`, `user_type`) VALUES ('1', 'user@user', 'sifra', '1');
+INSERT IGNORE INTO `diplomski`.`my_user` (`id`, `email`, `password`, `user_type`) VALUES ('2', 'lead@lead', 'sifra', '0');
